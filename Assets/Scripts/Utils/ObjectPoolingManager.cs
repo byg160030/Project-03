91a176ce227a23ad8ada8c9eefd44afa471978ee
Brice Yuuki Gibson <byg160030@utdallas.edu>
2020-11-20 03:49:35 -0600

    Gun and Bullets Added
    Player can now shoot.

---
+++ Assets/Scripts/Utils/ObjectPoolingManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolingManager : MonoBehaviour
+{
+
+    private static ObjectPoolingManager instance;
+    public static ObjectPoolingManager Instance {  get { return instance; } }
+
+    public GameObject bulletPrefab;
+    public int bulletAmount = 20;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject GetBullet()
+    {
+        Debug.Log("Hello!");
+        return null;
+    }
+}
